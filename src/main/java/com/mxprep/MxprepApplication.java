b1bda2eb4de052321ec56db534f8e7d0d28c09c6
Marko_Bogdanovic <marko_bogdanovic@epam.com>
2023-03-31 18:00:11 +0200

    Initial commit with basic project setup.

---
+++ src/main/java/com/mxprep/MxprepApplication.java
@@ -0,0 +1,13 @@
+package com.mxprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxprepApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MxprepApplication.class, args);
+    }
+
+}
